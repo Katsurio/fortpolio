951409ab5ddd8ccad66924bc3ff4e901b2348c30
John (Jef) Fisher <Fishe545@gmail.com>
2019-03-19 16:46:32 -0700

    fixed formatting

--- includes/experience.php
+++ includes/experience.php
@@ -7,79 +7,84 @@
                     <div class="timeline-body">
                         <article class="timeline-box right custom-box-shadow-2">
                             <div class="experience-info col-md-3 col-sm-5 match-height background-color-primary">
-											<span class="from text-color-dark text-uppercase">
-												From
-												<span class="font-weight-semibold">Feb 2018</span>
-											</span>
+                                <span class="from text-color-dark text-uppercase">
+                                    From
+                                    <span class="font-weight-semibold">Feb 2018</span>
+                                </span>
                                 <span class="to text-color-dark text-uppercase">
-												To
-												<span class="font-weight-semibold">Mar 2019</span>
-											</span>
+                                    To
+                                    <span class="font-weight-semibold">Mar 2019</span>
+                                </span>
                                 <p class="text-color-dark">(1 Year 1 Month)</p>
                                 <span class="company text-color-dark font-weight-semibold">
-												Tevora Business Solutions
-												<span class="company-location text-color-dark font-weight-normal text-uppercase">Irvine, CA</span>
-											</span>
+                                    Tevora Business Solutions
+                                    <span class="company-location text-color-dark font-weight-normal text-uppercase">Irvine, CA</span>
+                                </span>
                             </div>
                             <div class="experience-description col-md-9 col-sm-7 match-height background-color-light">
                                 <h4 class="text-color-dark font-weight-semibold">Security Solutions Analyst/Web Developer</h4>
-                                <p class="custom-text-color-2">Lead developer in charge of design, front-end, and
-                                    back-end development using PHP, LAMP, JS, jQuery, CSS, Bootstrap, Foundation, Java,
-                                    Apache Tomcat, Python, Flask, ASP.NET Core, C#, Vue.js, Firebase, and VM Ware.
-                                    Implemented a development schedule for application development. Created all design
-                                    documents (i.e. mockups and wireframes) for approval. Developed portals and landing
-                                    pages to spec from approved designs. Created the testing plan for development
-                                    projects.</p>
+                                <p class="custom-text-color-2">
+                                    Lead developer in charge of design, front-end, and back-end development using PHP,
+                                    LAMP, JS, jQuery, CSS, Bootstrap, Foundation, Java, Apache Tomcat, Python, Flask,
+                                    ASP.NET Core, C#, Vue.js, Firebase, and VM Ware. Implemented a development schedule
+                                    for application development. Created all design documents (i.e. mockups and
+                                    wireframes) for approval. Developed portals and landing pages to spec from approved
+                                    designs. Created the testing plan for development projects.
+                                </p>
                             </div>
                         </article>
                         <article class="timeline-box right custom-box-shadow-2">
                             <div class="experience-info col-md-3 col-sm-5 match-height background-color-primary">
-											<span class="from text-color-dark text-uppercase">
-												From
-												<span class="font-weight-semibold">Sep 2017</span>
-											</span>
+                                <span class="from text-color-dark text-uppercase">
+                                    From
+                                    <span class="font-weight-semibold">Sep 2017</span>
+                                </span>
                                 <span class="to text-color-dark text-uppercase">
-												To
-												<span class="font-weight-semibold">Feb 2018</span>
-											</span>
+                                    To
+                                    <span class="font-weight-semibold">Feb 2018</span>
+                                </span>
                                 <p class="text-color-dark">(6 Months)</p>
                                 <span class="company text-color-dark font-weight-semibold">
-												Tevora Business Solutions
-												<span class="company-location text-color-dark font-weight-normal text-uppercase">Lake Forest, CA</span>
-											</span>
+                                    Tevora Business Solutions
+                                    <span class="company-location text-color-dark font-weight-normal text-uppercase">Lake Forest, CA</span>
+                                </span>
                             </div>
                             <div class="experience-description col-md-9 col-sm-7 match-height background-color-light">
                                 <h4 class="text-color-dark font-weight-semibold">PHP Developer</h4>
-                                <p class="custom-text-color-2">Full stack development utilizing PHP, Javascript, jQuery,
-                                    Bootstrap, CSS, and HTML to securely build portals leveraging Okta's Identity and
-                                    Access Management APIs. Portals were developed with secure login, password/profile
-                                    updates, MFA and MFA factor resets, landing pages for
-                                    company apps, and any other custom functionality requested.</p>
+                                <p class="custom-text-color-2">
+                                    Full stack development utilizing PHP, Javascript, jQuery, Bootstrap, CSS, and HTML
+                                    to securely build portals leveraging Okta's Identity and Access Management APIs.
+                                    Portals were developed with secure login, password/profile updates, MFA and MFA
+                                    factor resets, landing pages for company apps, and any other custom functionality
+                                    requested.
+                                </p>
                             </div>
                         </article>
                         <article class="timeline-box right custom-box-shadow-2">
                             <div class="experience-info col-md-3 col-sm-5 match-height background-color-primary">
-											<span class="from text-color-dark text-uppercase">
-												From
-												<span class="font-weight-semibold">Sep 2016</span>
-											</span>
+                                <span class="from text-color-dark text-uppercase">
+                                    From
+                                    <span class="font-weight-semibold">Sep 2016</span>
+                                </span>
                                 <span class="to text-color-dark text-uppercase">
-												To
-												<span class="font-weight-semibold">Sep 2017</span>
-											</span>
+                                    To
+                                    <span class="font-weight-semibold">Sep 2017</span>
+                                </span>
                                 <p class="text-color-dark">(1 Year)</p>
                                 <span class="company text-color-dark font-weight-semibold">
-												Freelance
-												<span class="company-location text-color-dark font-weight-normal text-uppercase">Laguna Niguel, CA</span>
-											</span>
+                                    Freelance
+                                    <span class="company-location text-color-dark font-weight-normal text-uppercase">Laguna Niguel, CA</span>
+                                </span>
                             </div>
                             <div class="experience-description col-md-9 col-sm-7 match-height background-color-light">
                                 <h4 class="text-color-dark font-weight-semibold">Freelance Web Developer</h4>
-                                <p class="custom-text-color-2">Developed the "Our Communities" page for a custom homes
-                                    website. The page designs were developed from PSDs provided
-                                    by the client, and improvements were made to the app's overall responsiveness at all
-                                    breakpoints. Implemented a Braintree payment system for an application that
-                                    retrieved recipes according to a user's available ingredients on hand.</p>
+                                <p class="custom-text-color-2">
+                                    Developed the "Our Communities" page for a custom homes website. The page designs
+                                    were developed from PSDs provided by the client, and improvements were made to the
+                                    app's overall responsiveness at all breakpoints. Implemented a Braintree payment
+                                    system and Google OAuth login for an application that retrieved recipes according
+                                    to a user's available ingredients on hand.
+                                </p>
                             </div>
                         </article>
                         <div class="timeline-bar"></div>
